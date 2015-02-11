e6612a8806c95828a546ba7c4712780906be0736
Michiaki Ariga <chezou+github@gmail.com>
2015-02-11 21:08:32 +0900

    Initial commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/mizuyarilink_octopress/"
+http_images_path = "/mizuyarilink_octopress/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/mizuyarilink_octopress/fonts"
+css_dir = "public/mizuyarilink_octopress/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
